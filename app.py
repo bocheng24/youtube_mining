04629c9481a1dc42e9e178846aae2d8af21c25ec
bocheng24 <jeffcgp@hotmail.com>
2024-10-22 23:42:20 +0800

    get quota limitation by user input

--- app.py
+++ app.py
@@ -16,38 +16,44 @@ param_cfgs = {
 }
 
 def main():
+    limit = input('Enter quota limitation for this run:\n')
+
     quota = 0
-    limit = 20
+    limit = int(limit)
 
-    while quota < limit:
-        dt_postfix = datetime.now().strftime('%m%d%Y%H:%M:%S')
+    try:
+        while quota < limit:
+            dt_postfix = datetime.now().strftime('%m%d%Y%H:%M:%S')
 
-        outputnames = {
-            'search': f'valorant_searchdata_{dt_postfix}.json',
-            'videos': f'valorant_videodata_{dt_postfix}.json'
-        }
+            outputnames = {
+                'search': f'valorant_searchdata_{dt_postfix}.json',
+                'videos': f'valorant_videodata_{dt_postfix}.json'
+            }
 
-        search_params = load_params('search', param_cfgs['search'])
-        search_api = YoutubeAPI('search', API_KEY, search_params)
-        search_data = search_api.list()
+            search_params = load_params('search', param_cfgs['search'])
+            search_api = YoutubeAPI('search', API_KEY, search_params)
+            search_data = search_api.list()
 
-        sr = SearchResult(search_data)
-        next_page_params = sr.nextPageParams(search_params)
+            sr = SearchResult(search_data)
+            next_page_params = sr.nextPageParams(search_params)
 
-        video_res = []
+            video_res = []
 
-        for mapping in sr.vChIdMappings:
-            params = load_params('videos', param_cfgs['videos'], id = mapping['videoId'])
-            video_api = YoutubeAPI('videos', API_KEY, params)
+            for mapping in sr.vChIdMappings:
+                params = load_params('videos', param_cfgs['videos'], id = mapping['videoId'])
+                video_api = YoutubeAPI('videos', API_KEY, params)
 
-            video_data = video_api.list()
-            video_res.append(video_data)
+                video_data = video_api.list()
+                video_res.append(video_data)
+                quota += 1
+
+            output_json(next_page_params, param_cfgs['search'], base_folder='paramsConfigs/search')
+            output_json(sr.toDict(), outputnames['search'])
+            output_json(video_res, outputnames['videos'])
             quota += 1
 
-        output_json(next_page_params, param_cfgs['search'], base_folder='paramsConfigs/search')
-        output_json(sr.toDict(), outputnames['search'])
-        output_json(video_res, outputnames['videos'])
-        quota += 1
+    finally:
+        print(f'Total quota costs: {quota}')
 
 
 
